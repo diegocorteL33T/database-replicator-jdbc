310caba7a6e983fb20638a811228599ff332a4f2
Diego Corte <emptyyyxd@gmail.com>
2026-06-11 00:11:30 -0300

    feat: implementa ações da tela de direção

--- src/view/TelaReplicacaoDirecaoView.java
+++ src/view/TelaReplicacaoDirecaoView.java
@@ -2,6 +2,7 @@ package view;
 
 import database.dao.DirecaoDAO;
 import database.model.TB_REPLICACAO_DIRECAO;
+import database.model.TB_REPLICACAO_PROCESSO;
 
 import javax.swing.*;
 import java.awt.*;
@@ -17,7 +18,7 @@ public class TelaReplicacaoDirecaoView extends JFrame{
     private final DirecaoDAO dao;
 
     private JTextField txfId;
-    private JComboBox<TB_REPLICACAO_DIRECAO> cmbProcesso;
+    private JComboBox<TB_REPLICACAO_PROCESSO> cmbProcesso;
 
     private JTextField txfOrigem;
     private JTextField txfDestino;
@@ -144,15 +145,216 @@ public class TelaReplicacaoDirecaoView extends JFrame{
         getContentPane().add(chkHabilitado);
 
 
-    }
+        txfId.setEnabled(false);
+        cmbProcesso.setEnabled(false);
+        chkHabilitado.setEnabled(false);
+        txfOrigem.setEnabled(false);
+        txfUsuarioOrigem.setEnabled(false);
+        txfSenhaOrigem.setEnabled(false);
+        txfDestino.setEnabled(false);
+        txfUsuarioDestino.setEnabled(false);
+        txfSenhaDestino.setEnabled(false);
+        btnSalvar.setEnabled(false);
+        btnExcluir.setEnabled(false);
 
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(()-> {
+        btnAdicionar.addActionListener(e -> {
+
+            modoTela = ModoTela.INSERT;
+
+            txfOrigem.setText("");
+
+            if(cmbProcesso.getItemCount() > 0) cmbProcesso.setSelectedIndex(0);
+            chkHabilitado.setSelected(true);
+
+            txfOrigem.setText("");
+            txfUsuarioOrigem.setText("");
+            txfSenhaOrigem.setText("");
+            txfDestino.setText("");
+            txfUsuarioDestino.setText("");
+            txfSenhaDestino.setText("");
+
+            cmbProcesso.setEnabled(true);
+            chkHabilitado.setEnabled(true);
+            btnSalvar.setEnabled(true);
+            txfOrigem.setEnabled(true);
+            txfUsuarioOrigem.setEnabled(true);
+            txfSenhaOrigem.setEnabled(true);
+            txfDestino.setEnabled(true);
+            txfUsuarioDestino.setEnabled(true);
+            txfSenhaDestino.setEnabled(true);
+
+        });
+
+        btnSalvar.addActionListener(e -> {
             try {
-                new TelaReplicacaoDirecaoView().setVisible(true);
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
+                if(cmbProcesso.getSelectedItem() == null) {
+                    JOptionPane.showMessageDialog(null, "Selecione um processo");
+                    return;
+                }
+
+                if(txfOrigem.getText().trim().isEmpty()) {
+                    JOptionPane.showMessageDialog(this,"Informe a origem");
+                    return;
+                }
+                if(txfDestino.getText().trim().isEmpty()) {
+                    JOptionPane.showMessageDialog(this,"Informe a destino");
+                    return;
+                }
+                if(txfSenhaOrigem.getText().trim().isEmpty()) {
+                    JOptionPane.showMessageDialog(this,"Informe a senha de origem");
+                    return;
+                }
+                if(txfSenhaDestino.getText().trim().isEmpty()) {
+                    JOptionPane.showMessageDialog(this,"Informe a senha de destino");
+                    return;
+                }
+                if(txfUsuarioOrigem.getText().trim().isEmpty()) {
+                    JOptionPane.showMessageDialog(this,"Informe o usuario de origem");
+                    return;
+                }
+                if(txfUsuarioDestino.getText().trim().isEmpty()) {
+                    JOptionPane.showMessageDialog(this,"Informe o usuario de destino");
+                    return;
+                }
+
+                TB_REPLICACAO_PROCESSO pSel = (TB_REPLICACAO_PROCESSO) cmbProcesso.getSelectedItem();
+
+                TB_REPLICACAO_DIRECAO d = new TB_REPLICACAO_DIRECAO();
+                d.setProcesso_id(pSel.getId());
+                d.setHabilitado(chkHabilitado.isSelected());
+                d.setDirecao_origem(txfOrigem.getText());
+                d.setDirecao_destino(txfDestino.getText());
+                d.setUsuario_origem(txfUsuarioOrigem.getText());
+                d.setUsuario_destino(txfUsuarioDestino.getText());
+                d.setSenha_origem(txfSenhaOrigem.getText());
+                d.setSenha_destino(txfSenhaDestino.getText());
+
+                if (modoTela == ModoTela.INSERT) {
+                    dao.insert(d);
+                    JOptionPane.showMessageDialog(this, "Direção inserida com sucesso");
+                } else if (modoTela == ModoTela.UPDATE) {
+                    if (txfId.getText().trim().isEmpty()) {
+                        JOptionPane.showMessageDialog(this, "Id não preenchido");
+                        return;
+                    }
+                    d.setId((long) Integer.parseInt(txfId.getText()));
+                    dao.update(d);
+                    JOptionPane.showMessageDialog(this,"Direção atualizada com sucesso");
+                } else {
+                    JOptionPane.showMessageDialog(this,"Clique em ADICIONAR ou BUSCAR antes de salvar");
+                }
+
+                modoTela = ModoTela.NENHUM;
+                txfId.setEnabled(false);
+                cmbProcesso.setEnabled(false);
+                chkHabilitado.setEnabled(false);
+                txfOrigem.setEnabled(false);
+                txfUsuarioOrigem.setEnabled(false);
+                txfSenhaOrigem.setEnabled(false);
+                txfDestino.setEnabled(false);
+                txfUsuarioDestino.setEnabled(false);
+                txfSenhaDestino.setEnabled(false);
+                btnSalvar.setEnabled(false);
+                btnExcluir.setEnabled(false);
+
+
+            } catch (Exception ex) {
+                ex.printStackTrace();
+                JOptionPane.showMessageDialog(null, "Erro ao salvar:" +ex.getMessage());
             }
         });
+
+        btnExcluir.addActionListener(e -> {
+            try {
+                if (txfId.getText().trim().isEmpty()) {
+                    JOptionPane.showMessageDialog(this,"Insira o ID para excluir");
+                    return;
+                }
+                int op  = JOptionPane.showConfirmDialog(this,"Deseja realmente excluir?");
+                if (op != JOptionPane.YES_OPTION) return;
+                dao.delete(Long.parseLong(txfId.getText()));
+
+                JOptionPane.showMessageDialog(this,"Direção excluida com sucesso");
+
+                txfOrigem.setText("");
+                txfUsuarioOrigem.setText("");
+                txfSenhaOrigem.setText("");
+                txfDestino.setText("");
+                txfUsuarioDestino.setText("");
+                txfSenhaDestino.setText("");
+
+                cmbProcesso.setEnabled(false);
+                chkHabilitado.setEnabled(false);
+                btnSalvar.setEnabled(false);
+                btnExcluir.setEnabled(false);
+                txfOrigem.setEnabled(false);
+                txfUsuarioOrigem.setEnabled(false);
+                txfSenhaOrigem.setEnabled(false);
+                txfDestino.setEnabled(false);
+                txfUsuarioDestino.setEnabled(false);
+                txfSenhaDestino.setEnabled(false);
+
+
+            }   catch (Exception ex) {
+                ex.printStackTrace();
+                JOptionPane.showMessageDialog(null, "Erro ao excluir:" +ex.getMessage());
+            }
+
+        });
+
+        btnBuscar.addActionListener(e -> {
+            try {
+
+            ConsultaDirecaoDialog cdd = new ConsultaDirecaoDialog(this,dao);
+            cdd.setVisible(true);
+
+            TB_REPLICACAO_DIRECAO d = new TB_REPLICACAO_DIRECAO();
+
+            if (d == null) return;
+
+            modoTela = ModoTela.UPDATE;
+
+
+            txfId.setText(String.valueOf(d.getId()));
+            chkHabilitado.setSelected(d.isHabilitado());
+            txfUsuarioOrigem.setText(d.getUsuario_origem());
+            txfUsuarioDestino.setText(d.getUsuario_destino());
+            txfSenhaOrigem.setText(d.getSenha_origem());
+            txfSenhaDestino.setText(d.getSenha_destino());
+            txfOrigem.setText(d.getUsuario_origem());
+            txfDestino.setText(d.getUsuario_destino());
+
+            Long id = (Long) d.getProcesso_id();
+            for (int i = 0; i < cmbProcesso.getItemCount(); i++) {
+                TB_REPLICACAO_PROCESSO p = cmbProcesso.getItemAt(i);
+                if (p.getId() == id) {
+                    cmbProcesso.setSelectedIndex(i);
+                    break;
+                }
+            }
+
+            cmbProcesso.setEnabled(true);
+            chkHabilitado.setEnabled(true);
+            txfOrigem.setEnabled(true);
+            txfUsuarioOrigem.setEnabled(true);
+            txfSenhaOrigem.setEnabled(true);
+            txfDestino.setEnabled(true);
+            txfUsuarioDestino.setEnabled(true);
+            txfSenhaDestino.setEnabled(true);
+            btnSalvar.setEnabled(true);
+            btnExcluir.setEnabled(true);
+
+
+            } catch (Exception ex) {
+                ex.printStackTrace();
+                JOptionPane.showMessageDialog(null, "Erro ao buscar:" +ex.getMessage());
+            }
+        });
+
+        btnSalvar.addActionListener(e -> {
+
+        });
+
     }
+
 }
